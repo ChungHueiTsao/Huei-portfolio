bfe0871103500fba49e6fee848866b9e5ce3822e
HueiHu <chunghuei.tsao@gmail.com>
2022-03-18 04:03:20 +0800

    90%

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-$('.autumn.leaf')
-  .transition('fade up')
-;
+// $('.autumn.leaf')
+//   .transition('fade up')
+// ;
+
+// 漢堡選單按鈕轉換
+
+$(document).ready(function() {
+  $('.container').on('click',function() {
+    $(this).toggleClass("change");
+    // $(this).preventDefault(); //這邊用不到停止事件
+    
+    $(this).hasClass('change')?$('.menu').slideDown():$('.menu').slideUp();
+  })
+});
